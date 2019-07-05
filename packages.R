e384e9a702c018e6b93bf4db6f61321d5ee63e54
J Achar <17378875+JayAchar@users.noreply.github.com>
2019-07-06 00:25:21 +0930

    add devtools to image, add docker-compose.yml

--- packages.R
+++ packages.R
@@ -1,4 +1,5 @@
 # script defining r packages to install
 
+install.packages("devtools", repos = "http://cran.us.r-project.org")
 install.packages("naniar", repos = "http://cran.us.r-project.org")
 install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
